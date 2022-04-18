70fd67af583e5d13e493516c83c7a16ffff1172e
abatallan <alvarojosebr@outlook.com.ar>
2022-04-18 18:20:45 -0300

    Added Upload File action and assert in BasePage.java Added close and quit Webdriver actions

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,8 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import utilitys.Driver;
 
+import java.io.File;
 import java.time.Duration;
 
 public abstract class BasePage {
@@ -13,5 +17,25 @@ public abstract class BasePage {
     public BasePage(Driver driver) {
         this.driver = driver;
         wait = new WebDriverWait(driver.getWebdriver(), TIMEOUT);
+    }
+
+    public void addFileToField(By locator, String fileLocalPath) {
+        File file = new File(fileLocalPath);
+        String path = file.getAbsolutePath();
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        driver.getWebdriver().findElement(locator).sendKeys(path);
+    }
+
+    public void assertCorrectAddFile(By locator, String fileLocalPath) {
+        File file = new File(fileLocalPath);
+        String valueExpected = file.getName();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        String elementValue = driver.getWebdriver().findElement(locator).getAttribute("value");
+
+        Assert.assertTrue(
+                elementValue.contains(valueExpected),
+                "Error element value: '"+ elementValue +"' not contains expected value '" + valueExpected + "'"
+                );
     }
 }
--- src/test/java/pages/ExamplePage.java
+++ src/test/java/pages/ExamplePage.java
@@ -1,11 +1,21 @@
 package pages;
 
+import org.openqa.selenium.By;
 import utilitys.Driver;
 
 public class ExamplePage extends BasePage {
+    private final By uploadPhoto = By.cssSelector("input[name='photo']");
 
     public ExamplePage(Driver driver) {
         super(driver);
+    }
+
+    public void UploadFile(String fileLocalPath) {
+        addFileToField(uploadPhoto, fileLocalPath);
+    }
+
+    public void assertUploadFile(String fileLocalPath) {
+        assertCorrectAddFile(uploadPhoto, fileLocalPath);
     }
 
 }
