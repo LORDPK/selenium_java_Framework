974f5a70c1a0937c97375026a6f11d865ce09438
abatallan <alvarojosebr@outlook.com.ar>
2022-06-09 18:19:10 -0300

    Added ExecutionConfig class.

---
+++ src/test/java/data/Constants.java
@@ -0,0 +1,10 @@
+package data;
+
+import java.time.Duration;
+
+public class Constants {
+    public static final String CHROME = "CHROME";
+    public static final java.time.Duration TIMEOUT = Duration.ofSeconds(30);
+    public static long WAITSHORTTIME = 1000;
+    public static long REINTENTLIMIT = 50;
+}
---
+++ src/test/java/data/ExecutionConfig.java
@@ -0,0 +1,19 @@
+package data;
+
+public class ExecutionConfig {
+    public String browser;
+    public String URI;
+    public String FileLocalPath;
+    public String ChromeDriverPath;
+
+    /*
+    Execution Config constructor, for now only hardcode values.
+    In the future should get values from multiple config files
+     */
+    public ExecutionConfig() {
+        browser = Constants.CHROME;
+        URI = "https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm";
+        FileLocalPath = "files/file.txt";
+        ChromeDriverPath = "files/Drivers/Windows/chromedriver.exe";
+    }
+}
